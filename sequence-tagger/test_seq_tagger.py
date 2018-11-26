92e1c5130eefec656c40369cc8302cfcb93164c4
Eric Lief <ericlief@me.com>
2018-11-26 12:29:08 +0100

    test

--- sequence-tagger/test_seq_tagger.py
+++ sequence-tagger/test_seq_tagger.py
@@ -38,34 +38,38 @@ from flair.embeddings import CharLMEmbeddings, WordEmbeddings, StackedEmbeddings
         #mini_batch_size=32,
         #max_epochs=150)
 
-fh = "/home/liefe/data/pt/harem/data/HAREM"
-#cols = {1:"text", 2:"lemma", 3:"pos"}
-cols = {0:"text", 1:"ne"}    
+#fh = "/home/liefe/data/pt/harem/data/HAREM"
+##cols = {1:"text", 2:"lemma", 3:"pos"}
+#cols = {0:"text", 1:"ne"}    
+
+tag_type = "mwe"
+fh = "/home/liefe/data/pt/mwe"
+cols = {1:"text", 2:"lemma", 3:"upos", 4:"xpos", 5:"features", 6:"parent", 7:"deprel", 10:"mwe"}
+
+
 corpus = NLPTaskDataFetcher.fetch_column_corpus(fh, 
                                                 cols, 
                                                 train_file="train.txt",
                                                 dev_file="dev.txt", 
-                                                test_file="test.txt").downsample(.1)
+                                                test_file="test.txt") 
 
 
+# Load festText word embeddings 
+word_embedding = WordEmbeddings("/home/liefe/.flair/embeddings/cc.pt.300.kv")
+#word_embedding = WordEmbeddings("/home/lief/files/embeddings/cc.pt.300.kv")
 
 # Load Character Language Models (clms)
-clm_fw = CharLMEmbeddings("/home/liefe/lm/fwd/best-lm.pt")  
+clm_fw = CharLMEmbeddings("/home/liefe/lm/fw_p25/best-lm.pt")  
 clm_bw = CharLMEmbeddings("/home/liefe/lm/bw_p25/best-lm.pt")    
-#clm_fw = CharLMEmbeddings("/home/liefe/code/sequence-tagger/sequence-tagger/resources/taggers/language_models/fw2/best-lm.pt")  
-#clm_bw = CharLMEmbeddings("/home/liefe/code/sequence-tagger/sequence-tagger/resources/taggers/language_models/bw2/best-lm.pt")    
+#clm_fw = CharLMEmbeddings("/home/lief/lm/fw_p25/best-lm.pt")
+#clm_bw = CharLMEmbeddings("/home/lief/lm/bw_p25/best-lm.pt")
 
-# Load festText word embeddings
-#word_embedding = WordEmbeddings("en")
-word_embedding = WordEmbeddings("/home/liefe/.flair/embeddings/cc.pt.300.kv")
-
+print("getting embeddings")
 # Instantiate StackedEmbeddings
-#stacked_embedding = StackedEmbeddings(embeddings=[clm_fw, clm_bw])
-stacked_embedding = StackedEmbeddings(embeddings=[clm_fw, clm_bw, word_embedding])
-#dummy_sent = Sentence("O gato é negro.")
-#stacked_embedding.embed(dummy_sent)
-#embedding_dim = len(dummy_sent[0].get_embedding())
-#print("Embedding dim: ", embedding_dim)
+stacked_embedding = StackedEmbeddings(embeddings=[word_embedding, clm_fw, clm_bw])
+
+
+
 
 # What tag do we want to predict?
 #tag_type = "pos"
@@ -75,22 +79,29 @@ tag_type = "ne"
 tag_dict = corpus.make_tag_dictionary(tag_type=tag_type)
 
 
-## Construct the tagger
-#from flair.models import SequenceTagger
-#tagger = SequenceTagger(hidden_size=64,
-            #embeddings=stacked_embedding,
-            #tag_dictionary=tag_dict,
-            #tag_type=tag_type,
-            #use_crf=True)
+# Construct the tagger
+from flair.models import SequenceTagger
+tagger = SequenceTagger(hidden_size=256,
+            embeddings=stacked_embedding,
+            tag_dictionary=tag_dict,
+            tag_type=tag_type,
+            use_crf=True)
 
 
-#from flair.trainers import SequenceTaggerTrainer
+from flair.trainers import SequenceTaggerTrainer
 
-#trainer = SequenceTaggerTrainer(tagger, corpus)
+trainer = SequenceTaggerTrainer(tagger, corpus)
 
-train_data = corpus.train
-stacked_embedding.embed(train_data)
-for s in corpus.train:
-    for t in s:
-        print(t, t.get_tag(tag_type), t.embedding)
+trainer.train("resources/taggers/example-mwe",
+        learning_rate=.1,
+        mini_batch_size=32,
+        max_epochs=150,
+        embeddings_in_memory=False)
+        
+        
+#train_data = corpus.train
+#stacked_embedding.embed(train_data)
+#for s in corpus.train:
+    #for t in s:
+        #print(t, t.get_tag(tag_type), t.embedding)
     
